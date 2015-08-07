d51185a4372b6ab4b4d4a45c48d535c485f19ff8
aarose <aarose@users.noreply.github.com>
2015-08-07 12:21:08 -0400

    Added tests, tweaked api

--- togglwrapper/api.py
+++ togglwrapper/api.py
@@ -7,6 +7,17 @@ API_VERSION = 'v8'
 API_URL = '{base}/{version}'.format(base=BASE_URL, version=API_VERSION)
 
 
+def return_json_or_raise_error(func):
+    def inner(*args, **kwargs):
+        response = func(*args, **kwargs)
+        try:
+            return response.json()
+        except ValueError:
+            # JSON couldn't be decoded, raise status error
+            response.raise_for_status()
+    return inner
+
+
 class TogglObject(object):
     @property
     def full_uri(self):
@@ -19,10 +30,10 @@ class TogglObject(object):
 class User(TogglObject):
     uri = '/me'
 
+    @return_json_or_raise_error
     def get(self, related_data=False, since=None):
         """ Get the user associated with the current API token. """
-        response = requests.get(self.full_uri, auth=self.client.auth)
-        return response.json()
+        return requests.get(self.full_uri, auth=self.client.auth)
 
     def update(self, data):
         """ Update the fields. """
@@ -31,14 +42,7 @@ class User(TogglObject):
 
 class Client(object):
     def __init__(self, api_token, base_url=BASE_URL, version=API_VERSION):
-        # self.api_token = api_token
         self.api_url = '{base}/{version}'.format(base=base_url,
                                                  version=version)
         self.auth = HTTPBasicAuth(api_token, 'api_token')
-        self.user = User(self)
-
-    def test_connection(self):
-        """ Test for a successful connection. """
-        test_url = '{base}{uri}'.format(base=self.api_url, uri='test')
-        response = requests.get(test_url, auth=self.client.auth)
-        return response.json()
+        self.User = User(self)
--- togglwrapper/tests.py
+++ togglwrapper/tests.py
@@ -1,18 +1,32 @@
+import json
 import unittest
+
+from mock import patch
+from requests.exceptions import HTTPError
 
 import api
 
 
 class TestAPIMethods(unittest.TestCase):
+    def setUp(self):
+        with open('json/user_get.json') as json_data:
+            self.data = json.load(json_data)
+            json_data.close()
+
     def test_client(self):
         """ Should successfully establish the client. """
-        api_token = 'correct_token'
-        self.assertTrue(api.Client(api_token))
+        api_token = 'fake_token_1'
+        toggl = api.Client(api_token)
+        with patch.object(toggl, 'User') as MockedUser:
+            MockedUser.get.return_value = self.data
+            response = toggl.User.get()
+            self.assertTrue(response)
 
     def test_client_wrong_token(self):
         """ Should raise exception when wrong API token is provided. """
         wrong_api_token = 'wrong token'
-        self.assertRaises(Exception, api.Client, wrong_api_token)
+        toggl = api.Client(wrong_api_token)
+        self.assertRaises(HTTPError, toggl.User.get)
 
 
 if __name__ == '__main__':
