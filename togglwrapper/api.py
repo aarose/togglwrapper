069678780b240878ace57b1d7a765543b921d134
aarose <aarose@users.noreply.github.com>
2015-08-11 22:38:58 -0400

    Fleshed out api more

--- togglwrapper/api.py
+++ togglwrapper/api.py
@@ -26,6 +26,28 @@ class TogglObject(object):
     def __init__(self, client):
         self.client = client
 
+    @return_json_or_raise_error
+    def get(self, object_id=None):
+        """ Get the array of objects, or a specific instance by ID. """
+        uri = self.full_uri
+        if object_id is not None:
+            uri = '{uri}/{object_id}'.format(uri=uri, object_id=object_id)
+        return requests.get(uri, auth=self.client.auth)
+
+    @return_json_or_raise_error
+    def create(self, data):
+        """ Create a new instance of the object type. """
+        return requests.post(self.full_uri, data=data, auth=self.client.auth)
+
+    @return_json_or_raise_error
+    def update(self, object_id, data):
+        uri = '{uri}/{object_id}'.format(self.full_uri, object_id)
+        return requests.put(uri, auth=self.client.auth)
+
+    @return_json_or_raise_error
+    def delete(self, object_id):
+        return requests.delete(uri, auth=self.client.auth)
+
 
 class User(TogglObject):
     uri = '/me'
@@ -40,9 +62,51 @@ class User(TogglObject):
         pass
 
 
+class Clients(TogglObject):
+    uri = '/clients'
+
+
+class Workspaces(TogglObject):
+    uri = '/workspaces'
+
+
+class Projects(TogglObject):
+    uri = '/projects'
+
+
+class ProjectUsers(TogglObject):
+    uri = '/project_users'
+
+
+class Tags(TogglObject):
+    uri = '/tags'
+
+
+class Tasks(TogglObject):
+    uri = '/tasks'
+
+
+class TimeEntries(TogglObject):
+    uri = '/time_entires'
+
+
+class WorkspaceUsers(TogglObject):
+    uri = '/workspace_users'
+
+
 class Client(object):
     def __init__(self, api_token, base_url=BASE_URL, version=API_VERSION):
         self.api_url = '{base}/{version}'.format(base=base_url,
                                                  version=version)
         self.auth = HTTPBasicAuth(api_token, 'api_token')
         self.User = User(self)
+
+    def signup(self):
+        """ Signup a new user. """
+        uri = '{}/signups'.format(self.api_url)
+        requests.post(uri, auth=self.auth)
+
+    def reset_token(self):
+        """ Delete the current API Token and use a new token. """
+        uri = '{}/reset_token'.format(self.api_url)
+        requests.post(uri, auth=self.auth)
