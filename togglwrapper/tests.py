8b198260693d6584fd82eb2e828fe580d3c254c4
aarose <aarose@users.noreply.github.com>
2015-08-21 00:30:33 -0400

    Revamped tests to use responses and mock at the right level

--- togglwrapper/tests.py
+++ togglwrapper/tests.py
@@ -1,51 +1,102 @@
 import json
 import unittest
 
-from mock import patch
+import responses
 
 import api
 from errors import AuthError
 
 
-class TestAPIMethods(unittest.TestCase):
+FAKE_TOKEN = 'fake_token_1'
+
+
+class TestTogglBase(unittest.TestCase):
+    """ Class to establish utility methods for Test classes. """
+
+    api_token = FAKE_TOKEN
+    focus_class = None
 
     def setUp(self):
-        self.api_token = 'fake_token_1'
         self.toggl = api.Toggl(self.api_token)
 
-    def _get_json(self, filename):
-        """ Return the JSON data contained in the given filename. """
+    def get_json(self, filename):
+        """ Return the JSON data contained in the given filename as a dict. """
         with open('json/{}.json'.format(filename)) as json_file:
-            json_data = json.load(json_file)
+            json_dict = json.load(json_file)
             json_file.close()
-        return json_data
+        return json.dumps(json_dict)
 
-    def test_client(self):
-        """ Should successfully establish the client. """
-        # TODO: patch futher upstream - in requests, probably.
-        with patch.object(self.toggl, 'User') as MockedUser:
-            MockedUser.get.return_value = self._get_json('user_get')
-            response = self.toggl.User.get()
-        self.assertTrue(response)
-        self.assertEqual(response['data']['api_token'], self.api_token)
+    @property
+    def full_url(self):
+        return self.toggl.api_url + self.focus_class.uri
 
-    def test_client_wrong_token(self):
+
+class TestToggl(TestTogglBase):
+
+    @responses.activate
+    def test_wrong_token(self):
         """ Should raise exception when wrong API token is provided. """
+        full_url = self.toggl.api_url + self.toggl.User.uri
+        responses.add(responses.GET, full_url, status=403)
         self.assertRaises(AuthError, self.toggl.User.get)
+        self.assertEqual(len(responses.calls), 1)
 
-    def test_client_create(self):
+
+class TestClients(TestTogglBase):
+    focus_class = api.Clients
+
+    @responses.activate
+    def test_create(self):
         """ Should create a new Client. """
-        with patch.object(self.toggl, 'Clients') as MockedClients:
-            MockedClients.create.return_value = self._get_json('client_create')
-            response = self.toggl.Clients.create()
-        self.assertTrue(response)
+        responses.add(
+            responses.POST,
+            self.full_url,
+            body=self.get_json('client_create'),
+            status=200,
+            content_type='application/json'
+        )
 
-    def test_client_get(self):
-        """ Should get a specific Client by ID. """
-        with patch.object(self.toggl, 'Clients') as MockedClients:
-            MockedClients.get.return_value = self._get_json('client_get')
-            response = self.toggl.Clients.get()
+        new_client_data = {"client": {"name": "Very Big Company", "wid": 777}}
+        response = self.toggl.Clients.create(new_client_data)
         self.assertTrue(response)
+        self.assertEqual(len(responses.calls), 1)
+
+    @responses.activate
+    def test_get_by_id(self):
+        """ Should get a specific Client by ID. """
+        client_id = 1239455
+        full_url = '{url}/{id}'.format(url=self.full_url, id=client_id)
+        responses.add(
+            responses.GET,
+            full_url,
+            body=self.get_json('client_get'),
+            status=200,
+            content_type='application/json'
+        )
+
+        response = self.toggl.Clients.get(id=client_id)
+        self.assertTrue(response)
+        self.assertEqual(len(responses.calls), 1)
+
+
+class TestUser(TestTogglBase):
+    focus_class = api.User
+
+    @responses.activate
+    def test_get(self):
+        """ Should successfully establish the client. """
+        responses.add(
+            responses.GET,
+            self.full_url,
+            body=self.get_json('user_get'),
+            status=200,
+            content_type='application/json'
+        )
+
+        response = self.toggl.User.get()
+
+        self.assertTrue(response)
+        self.assertEqual(len(responses.calls), 1)
 
 
 if __name__ == '__main__':
